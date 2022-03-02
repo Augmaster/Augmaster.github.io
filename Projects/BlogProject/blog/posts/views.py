a2817ff0f978a4e5a057207bd701206e22e45b46
Augmaster <augustin.renard@hec.ca>
2022-03-02 16:16:29 -0500

    blog post

--- Projects/BlogProject/blog/posts/views.py
+++ Projects/BlogProject/blog/posts/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from .models import Post
 
 
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
+
+
+def post(request, pk):
+    posts = Post.objects.get(id=pk)
+    return render(request, 'post.html', {'posts': posts})
 
 
 def register(request):
