f5a54717db957c142ad326a6e48c591195339ced
Arenard <augustin.renard@hec.ca>
2023-02-04 23:45:27 -0500

    secret project

---
+++ secret/script.js
@@ -0,0 +1,16 @@
+function heartFunction(){
+    let heartContainer = document.getElementsByClassName('heart-Container')[0]
+    heartContainer.classList.toggle('broken')
+}
+
+$(document).ready(function () {
+    $('.container').mouseenter(function () {
+        $('.card').stop().animate({
+            top: '-90px'
+        }, 'slow');
+    }).mouseleave(function () {
+        $('.card').stop().animate({
+            top: 0
+        }, 'slow');
+    });
+});
